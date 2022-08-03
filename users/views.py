b460acf25b097e8448c4c2a210bbec669ff064b5
vitaliyvoloshyn <vitaliy.srt1945@gmail.com>
2022-08-03 15:55:59 +0300

    create basic API

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import CustomUser
+from .serializers import CustomUserModelSerializer
+
+
+class CustomUserModelViewSet(ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserModelSerializer
