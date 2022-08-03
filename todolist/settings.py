2f5e16e3d631158a34fd1b1052e890d43a3a909d
vitaliyvoloshyn <vitaliy.srt1945@gmail.com>
2022-08-03 15:48:14 +0300

    create CustomUser model

--- todolist/settings.py
+++ todolist/settings.py
@@ -115,6 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# users
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
