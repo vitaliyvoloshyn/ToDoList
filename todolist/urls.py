110cade24f9672710bae9e1cf577dc43d4747757
vitaliyvoloshyn <vitaliy.srt1945@gmail.com>
2022-09-13 14:05:51 +0300

    -

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,15 +2,14 @@
 # from rest_framework.schemas import get_schema_view
 # from django.views.generic import TemplateView
 
+from django.contrib import admin
+from django.urls import path, include
+from django.urls import re_path
 # Open-API swagger с использованием библиотеки drf-yasg
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from graphene_django.views import GraphQLView
 from rest_framework import permissions
-from django.urls import re_path
-
-from django.contrib import admin
-from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
@@ -43,6 +42,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path("graphql/", GraphQLView.as_view(graphiql=True)),
 
     # Open-API swagger с использованием библиотеки drf-yasg
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
