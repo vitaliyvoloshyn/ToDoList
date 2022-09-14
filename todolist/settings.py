1485af0789a83cc90d3580cd0a80a275e5633e88
vitaliyvoloshyn <vitaliy.srt1945@gmail.com>
2022-09-14 15:03:08 +0300

    lesson 10 done

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "graphene_django"
+    "graphene_django",
     'drf_yasg',
     'rest_framework',
     'corsheaders',
